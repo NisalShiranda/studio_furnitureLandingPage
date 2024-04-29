e4880edbee806bc4529a411e387af9ea1e5786cd
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-04-29 10:08:53 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Navbar from './pages/Navbar/Navbar'
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Nisal Shiranda</h1>
+      <Navbar />
     </>
   )
 }
---
+++ src/pages/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <h1 className='text-yellow-600'>Nisal Shiranda</h1>
+    </div>
+  )
+}
+
+export default Navbar
