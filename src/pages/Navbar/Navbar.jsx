5f89c0120654576efde4e4774ff6adddc080e955
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-04-29 19:55:05 +0530

    ss

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import "../Navbar/Navbar.css"
 
 function Navbar() {
   return (
-    <div>
-        <h1 className='text-yellow-600'>Nisal Shiranda</h1>
-    </div>
+    <>
+        <div className="navbar ">
+            <div className="navbar-logo">
+                <h1 className="font-bold text-2xl text-[#F4D03F]">
+                <span className="text-[#2E4053]">S</span>tudio.
+                </h1>
+            </div>
+            <div className = "navbar-item">
+                <p>Home</p>
+                <p>Products</p>
+                <p>Sales</p>
+                <p>Contacts</p>
+            </div>
+            <div className="navbar-signin"></div>
+
+        </div>
+
+
+
+
+    </>
   )
 }
 
