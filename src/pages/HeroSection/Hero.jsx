31ae11d8ebc72602e9f1021ed764bde7d67a16b5
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-05-03 08:42:04 +0530

    ss

--- src/pages/HeroSection/Hero.jsx
+++ src/pages/HeroSection/Hero.jsx
@@ -1,13 +1,18 @@
 import './Hero.css'
+import HeroImage from '../../../public/herochair.png'
 
 function Hero() {
   return (
     <>
         <div className="hero">
-            <div className="hero-text">
-                <p>Trendy Collection</p>
+            <div className="hero-text ">
+                <p>TRENDY COLLECTION</p>
+                <p className="text-[4rem] font-semibold">Make Your Interior Unique & Modern.</p>
+                <p className='text-[1.5rem] font-medium'>Turn your room with panto into a lot more minimalist and modern.</p>
             </div>
-            <div className="hero-image"></div>
+            <div className="hero-image">
+                <img src={HeroImage} alt="hero-image"></img>
+            </div>
         </div>
     </>
   )
