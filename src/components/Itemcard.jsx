cacdf0877477a124396e5c30fe7527ecd15ec2ea
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-05-10 08:43:27 +0530

    card components

--- src/components/Itemcard.jsx
+++ src/components/Itemcard.jsx
@@ -1,11 +1,45 @@
+
+import PropTypes from 'prop-types';
 import "./Itemcars.css"
 
-function Itemcard() {
-  return (
-    <div className="itemCard">
-        Nisal Shiranda
-    </div>
-  )
-}
 
-export default Itemcard
+export const Itemcard = ({
+   imgSrc,
+  // imgAlt,
+  // title,
+  // description,
+}) => {
+
+  return(
+
+    <>
+      <div className="card-container">
+        <div className="flex justify-center items-center bg-[#EEEEEE]"><img className="w-[75%]"src={imgSrc} alt=""></img></div>
+        <div className="card-text">
+          <div className="card-description"></div>
+          <div className="card-star"></div>
+          <div className="price">
+            <div className="price-text"></div>
+            <div className="add-icon"></div>
+          </div>
+        </div>
+        
+      </div>
+    </>
+
+
+  )
+    
+};
+
+Itemcard.propTypes = {
+
+  imgSrc: PropTypes.string,
+//   // imgSrc: PropTypes.string,
+//   // imgAlt: PropTypes.string,
+//   // title: PropTypes.string,
+//   // description: PropTypes.string,
+ };
+
+
+export default Itemcard;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,5 +1,9 @@
 import "./Products.css"
-import Itemcard from "../../components/Itemcard"
+import {Itemcard} from "../../components/Itemcard"
+import Item1 from "../../../public/item1.png"
+import Item2 from "../../../public/item2.png"
+import Item3 from "../../../public/item3.png"
+import Item4 from "../../../public/item4.png"
 
 function Products() {
   return (
@@ -9,11 +13,19 @@ function Products() {
             <p className="text-[1rem] text-center">There are many variations of packages of sofas and other stufs.<br></br>but the majority have suffed.</p>
         </div>
 
-        <div className="productCard grid grid-cols-4 gap-5">
-            <div><Itemcard /></div>
-            <div><Itemcard /></div>
-            <div><Itemcard /></div>
-            <div><Itemcard /></div>
+        <div className="productCard pt-4 grid grid-cols-4 gap-2">
+            <div className="card">
+              <Itemcard imgSrc={Item1} />
+            </div>
+            <div className="card">
+              <Itemcard imgSrc={Item2} />
+            </div>
+            <div className="card">
+              <Itemcard imgSrc={Item3} />
+            </div>
+            <div className="card">
+              <Itemcard imgSrc={Item4} />
+            </div>
 
         </div>
     </div>
